82cd82bf8d6c7e68310e4b77d99b2ec748fa2c08
Emily Wade <emilywade314@gmail.com>
2024-06-25 08:31:35 +0000

    create function for generate a random word of given length

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Wordle game
+Guess the 5 letter word!
+"""
+
+import random
+
+def generate_random_word(length):
+    """
+    Function to generate a random word of given length.
+    Word selected from list provided words_list.txt (ref: https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt)
+    """
+    with open("words_list.txt") as f:
+        all_words = f.read().splitlines()
+        return random.choice([i for i in all_words if len(i) == length])
+
+word = generate_random_word(5)
+print(word)
