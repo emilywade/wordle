0466f4fb29aecb2420755fe6f2fb8f7b0b19a449
Emily Wade <emilywade314@gmail.com>
2024-06-25 08:57:41 +0000

    create class Board to initialise game play

--- run.py
+++ run.py
@@ -5,6 +5,25 @@ Guess the 5 letter word!
 
 import random
 
+class Board:
+    """
+    Board class which initialises game play.
+    Sets the length of the word, number of guesses allowed, number of guesses used
+    so far, and the list of guesses taken.
+    Stores the method for creating the board. 
+    """
+    def __init__(self, length, guesses_allowed):
+        self.length = length
+        self.guesses_allowed = guesses_allowed
+        self.board = ["_" for x in range(length)]
+        self.guesses_used = 0
+        self.guesses = []
+
+    def print_board(self):
+        print(" ".join(self.board))
+
+
+
 def generate_random_word(length):
     """
     Function to generate a random word of given length.
@@ -15,4 +34,7 @@ def generate_random_word(length):
         return random.choice([i for i in all_words if len(i) == length])
 
 word = generate_random_word(5)
-print(word)
+# print(word)
+
+
+initial = Board(6, 5).print_board()
