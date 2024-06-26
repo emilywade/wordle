8354267ea74c53607ff9a2be02b78554ad84723c
Emily Wade <emilywade314@gmail.com>
2024-06-26 15:46:34 +0000

    creating main game play function

--- run.py
+++ run.py
@@ -10,12 +10,12 @@ class Board:
     Board class which initialises game play.
     Sets the length of the word, number of guesses allowed, number of guesses used
     so far, and the list of guesses taken.
-    Stores the method for creating the board. 
+    Stores the method for creating the board and generating the random word. 
     """
     def __init__(self, length, guesses_allowed):
-        self.length = length
-        self.guesses_allowed = guesses_allowed
-        self.board = ["_" for x in range(length)]
+        self.length = int(length)
+        self.guesses_allowed = int(guesses_allowed)
+        self.board = ["_" for x in range(int(length))]
         self.guesses_used = 0
         self.guesses = []
 
@@ -33,10 +33,31 @@ class Board:
             print(chosen_word)
             return chosen_word
 
-# word = generate_random_word(5)
-# print(word)
+
+# board = Board(6, 5)
+# board.print_board()
+# board.generate_random_word()
+
+def game_setup():
+    print("Welcome to Wordle!\n")
+    player_name = input("What is your name?\n")
+    print(f"Thanks for playing, {player_name}")
+    print("INSTRUCTIONS OF GAME PLAY TO GO HERE")
+    length = input("How many letters long would you like your random word to contain? You can choose 4, 5 or 6.")
+    print(f"You have choosen {length} letter words.")
+    guesses_allowed = input("How many guesses would you like to have? You can choose 4, 6 or 8.")
+    print(f"You have choosen to have no more than {guesses_allowed} guesses.")
+    return player_name, length, guesses_allowed
+
+# game_setup()
 
 
-board = Board(6, 5)
-board.print_board()
-board.generate_random_word()
+def main():
+    """
+    Main game play function.
+    """
+    player_name, length, guesses_allowed = game_setup()
+    board = Board(length, guesses_allowed)
+    board.print_board()
+
+main()
